e6e4dee4be2c24de8b0e699ad9373c136da70721
arata-e <47027066+arata-e@users.noreply.github.com>
2025-10-21 22:28:59 +0300

    Updated TicketTable.vue

--- app/components/TicketTable.vue
+++ app/components/TicketTable.vue
@@ -1,41 +1,5 @@
-<template>
-  <div class="p-5">
-    <div v-if="loading" class="text-center py-5 text-gray-600">
-      Loading...
-    </div>
-
-    <B24Alert v-if="error" color="error" class="m-5">
-      Error: {{ error }}
-    </B24Alert>
-
-    <B24TableWrapper v-if="!loading && !error" bordered zebra row-hover>
-      <table class="w-full">
-        <thead>
-          <tr>
-            <th class="w-24 p-3 text-left font-semibold">ID</th>
-            <th class="p-3 text-left font-semibold">Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="ticket in tickets" :key="ticket.id">
-            <td class="p-3">{{ ticket.id }}</td>
-            <td class="p-3">
-              <B24Link
-                :href="`https://tp.point.online/tickets?id=${ticket.id}`"
-                target="_blank"
-              >
-                {{ ticket.name }}
-              </B24Link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </B24TableWrapper>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 
 interface Ticket {
   id: number
@@ -45,6 +9,14 @@ interface Ticket {
 const tickets = ref<Ticket[]>([])
 const loading = ref(true)
 const error = ref<string | null>(null)
+
+const tableData = computed(() => {
+  return tickets.value.map(ticket => ({
+    id: ticket.id.toString(),
+    name: ticket.name,
+    link: `https://tp.point.online/tickets?id=${ticket.id}`
+  }))
+})
 
 const fetchTickets = async () => {
   try {
@@ -76,3 +48,25 @@ onMounted(() => {
   fetchTickets()
 })
 </script>
+
+<template>
+  <div class="p-5">
+    <B24Alert v-if="error" color="error" class="mb-5">
+      Error: {{ error }}
+    </B24Alert>
+
+    <B24Table
+      :loading="loading"
+      loading-color="air-primary"
+      loading-animation="loading"
+      :data="tableData"
+      class="flex-1"
+    >
+      <template #name-data="{ row }">
+        <B24Link :href="row.link" target="_blank">
+          {{ row.name }}
+        </B24Link>
+      </template>
+    </B24Table>
+  </div>
+</template>
